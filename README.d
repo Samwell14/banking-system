0ad6e6ee7411c534a2b52dd6cc4941078fd4f7fb
Samwell14 <samuelmesfin125@gmail.com>
2024-03-24 12:58:25 +0300

    delete mistaken file "README.d"

--- README.d
+++
@@ -1,5 +0,0 @@
------------------------------------------------------------------------------
------------------------------------------------------------------------------
-
-This is a C++ console based banking system that users can utilize almost every
-feature in a fully-fledged banking application.
